0d80257fed746edc3da60952a288d1364a45f6bd
james <james.danylik@gmail.com>
2018-07-30 22:48:24 -0700

    Add pupeteer testing setup

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+const { defaults } = require("jest-config")
+
+module.exports = {
+  preset: "jest-puppeteer",
+  testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, ".cache"],
+  verbose: true
+}
---
+++ test/homepage.spec.js
@@ -0,0 +1,33 @@
+const puppeteer = require("puppeteer")
+const { port } = require("../jest-puppeteer.config").server
+
+const siteRoot = `http://localhost:${port}`
+
+describe("Homepage", () => {
+  let browser = ""
+  let page = ""
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({args: ["--no-sandbox", "--disable-setuid-sandbox"]})
+    page = await browser.newPage()
+
+    page.emulate({
+      viewport: {
+	width: 500,
+	height: 2400
+      },
+      userAgent: ""
+    })
+
+    await page.goto("http://localhost:9000")
+  })
+
+  afterAll(async () => {
+    browser.close()
+  })
+
+  test("Site title is visible", async () => {
+    const t = await page.title()
+    expect(t).toBe("james.danylik.com")
+  })
+})
