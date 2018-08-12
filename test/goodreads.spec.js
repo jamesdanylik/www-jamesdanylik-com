fd5f897c84eae56bc9a9119877b9af1b6eb1be9f
james <james.danylik@gmail.com>
2018-08-11 22:41:40 -0700

    Lint goodreads.spec.js

--- test/goodreads.spec.js
+++ test/goodreads.spec.js
@@ -8,6 +8,8 @@ jest.setTimeout(30000)
 describe("Goodreads Source Plugin", () => {
   let browser = ""
   let page = ""
+  let data = ""
+  let t = ""
 
   beforeAll(async () => {
     browser = await puppeteer.launch({args: ["--no-sandbox", "--disable-setuid-sandbox"]})
@@ -22,37 +24,28 @@ describe("Goodreads Source Plugin", () => {
     })
 
     await page.goto(testRoot)
+    const rawText = await page.$eval("#test-data", e => e.innerHTML )
+    data = JSON.parse(rawText)
+    t = await page.title()
   })
 
   afterAll(async () => {
     browser.close()
   })
 
-  const getData = async () => {
-    const rawText = await page.$eval("#test-data", e => e.innerHTML )
-    return JSON.parse(rawText)
-  }
-
   test("Site title tag matches expected (test page exists)", async () => {
-    const t = await page.title()
     expect(t).toBe("gatsby-source-goodreads test page")
   })
 
   test("Inner HTML is parseable JSON", async ()  => {
-    const data = await getData()
-
     expect(data.length).toBeGreaterThan(0)
   })
 
   test("User has at least 3 shelves", async () => {
-    const data = await getData()
-
     expect(data.length).toBeGreaterThan(2)
   })
 
   test("Each review on a shelf has a book", async () => {
-    const data = await getData()
-
     data.forEach(n => {
       n.node.reviews.forEach(review => {
 	expect(typeof review.book).toBe("object")
@@ -61,8 +54,6 @@ describe("Goodreads Source Plugin", () => {
   })
 
   test("Each book has a title", async () => {
-    const data = await getData()
-
     data.forEach(n => {
       n.node.reviews.forEach(review => {
 	expect(typeof review.book.title).toBe("string")
@@ -71,8 +62,6 @@ describe("Goodreads Source Plugin", () => {
   })
 
   test("Each book has an author node", async () => {
-    const data = await getData()
-
     data.forEach(n => {
       n.node.reviews.forEach(review => {
 	expect(typeof review.book.authors).toBe("object")
@@ -81,8 +70,6 @@ describe("Goodreads Source Plugin", () => {
   })
 
   test("Each author has a name", async () => {
-    const data = await getData()
-
     data.forEach(n => {
       n.node.reviews.forEach(review => {
 	review.book.authors.forEach(author => {
