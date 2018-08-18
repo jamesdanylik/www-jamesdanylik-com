a8cd46646e0e0be8249f7dd550dc97900acab79c
james <james.danylik@gmail.com>
2018-08-17 23:16:30 -0700

    Addded 4 Man overall graph

--- src/pages/mahjong.jsx
+++ src/pages/mahjong.jsx
@@ -31,49 +31,33 @@ class MahjongPage extends Component {
   }
 
   async fetchSheetScores() {
+    const data = {}
     const MAHJONG_CSV_URL =
       "https://docs.google.com/spreadsheets/d/e/2PACX-1vTnhaUOyUmX4o7bQf1nUcWNr37WcQR80S7_fU4_exvwXBXU7QXTHVtwaJv5Q2qWlk6oEDH2jDDEW3Vw/pub?gid=869579873&single=true&output=csv";
     const TENHOU_JSON_URL =
       "https://proxy.danylik.com/nodocchi/api/listuser.php?name=0ddba11";
 
     // Process response from House Google Sheet
-    parse(await (await fetch(MAHJONG_CSV_URL)).text(), (err, data) => {
+    parse(await (await fetch(MAHJONG_CSV_URL)).text(), (err, csv) => {
       if (err) {
-        console.log("Error parsing csv");
+        this.setState({error: "Failed to parse house CSV!"})
         return;
       }
 
-      this.setState({ loaded: "CSV parsed okay. Starting calculations..." });
+      this.setState({ status: "CSV parsed okay. Starting calculations..." });
 
-      // HOUSE GAMES
-      // split table into 3 man games and four man games
-      // delete empty rows from whichever was shorter
-      // table structure:
-      //  3manDate 3manPlay1 3ManPlay2 3ManPlay3 3ManScore1 3ManScore2 3ManScore3
-      //  4ManDate 4ManPlay1 4ManPlay2 4ManPlay3 4ManPlay4 4ManScore1 4ManScore2 4ManScore3 4ManScore4
-      // establish seasons?
-      // create player table
-      // for each row (game)
-      //  1. calculate global score
-      //  2. calculate season score
-      //  3. calculate dan point score
-      //  4. calculate R score
-
-      const houseBalance = {}
-      const housePlayers = {}
-      const houseSeasons = []
 
       // These will be set to the date of the first and last games
       let houseStartDate = new Date(Date.parse("07-16-2089"))
       let houseEndDate = new Date(Date.parse("07-16-1989"))
 
-      const house4Man = []
-      const house3Man = []
+      const table4Man = []
+      const table3Man = []
 
       // First pass:
       //   Collect player names + tenhou names
       //   Split into logical tables
-      data.forEach(row => {
+      csv.forEach(row => {
 	const [pHouseName, pTenhouName ] = row.slice(-2)
 	const [seasonStart, seasonEnd ] = row.slice(-4, -2)
 	const game4Man = row.slice(7, 16)
@@ -81,24 +65,17 @@ class MahjongPage extends Component {
 
 
 	if( pHouseName !== "" && pHouseName !== "House Player" ) {
-	  if( housePlayers.hasOwnProperty( pHouseName ) ) {
-	    if( ! housePlayers[pHouseName].hasOwnProperty("tenhou") ) {
-	      housePlayers[pHouseName].tenhou = [ pTenhouName ]
-	    } else {
-	      housePlayers[pHouseName].tenhou.push(pTenhouName)
-	    }
+	  if( data.hasOwnProperty( pHouseName ) ) {
+	    data[pHouseName][pTenhouName] = {}
 	  } else {
-	    housePlayers[pHouseName] = {
-	      tenhou: [ pTenhouName ]
+	    data[pHouseName] = {
+	      house: {},
+	      [ pTenhouName ]: {}
 	    }
 	  }
 	}
 
 	if( seasonStart !== "" && seasonStart !== "Start Date" ) {
-	  houseSeasons.push({
-	    start: new Date(Date.parse(seasonStart)),
-	    end: new Date(Date.parse(seasonEnd))
-	  })
 	}
 
 	if( game4Man[0] !== "" && game4Man[0] !== "Time" ) {
@@ -111,13 +88,35 @@ class MahjongPage extends Component {
 	    houseStartDate = gameDate
 	  }
 
+
 	  game4Man.slice(1, 5).forEach(player => {
-	    if( !housePlayers.hasOwnProperty(player) ) {
-	      housePlayers[player] = {}
+	    if( data.hasOwnProperty(player) ) {
+	      if ( data[player].hasOwnProperty("house") ) {
+		data[player].house.four = {
+		  overall: {},
+		  seasons: []
+		}
+	      } else {
+		data[player].house = { 
+		  four: {
+		    overall: {},
+		    seasons: []
+		  }
+		}
+	      }
+	    } else {
+	      data[player] = {
+		house: {
+		  four: {
+		    overall: {},
+		    seasons: []
+		  }
+		}
+	      }
 	    }
 	  })
 
-	  house4Man.push(game4Man)
+	  table4Man.push(game4Man)
 	}
 
 	if ( game3Man[0] !== "" && game3Man[0] !== "Time" ) {
@@ -131,34 +130,54 @@ class MahjongPage extends Component {
 	  }
 
 	  game3Man.slice(1, 4).forEach(player => {
-	    if( !housePlayers.hasOwnProperty(player) ) {
-	      housePlayers[player] = {}
+	    if( data.hasOwnProperty(player) ) {
+	      if( data[player].hasOwnProperty("house") ){
+		data[player].house.three = {
+		  overall: {},
+		  seasons: []
+		}
+	      } else {
+		data[player].house = {
+		  three: {
+		    overall: {},
+		    seasons: []
+		  }
+		}
+	      }
+	    } else {
+	      data[player] = {
+		house: {
+		  three: {
+		    overall: {},
+		    seasons: []
+		  }
+		}
+	      }
 	    }
 	  })
 
-	  house3Man.push(game3Man)
+	  table3Man.push(game3Man)
 	}
       })
 
-      house3Man.forEach(game => {
+      table3Man.forEach(game => {
 	const date = new Date(Date.parse(game[0]))
 
 	for( let i = 0; i < 3; i += 1 ) {
 	  const player = game[1+i]
 	  const score = Number(game[4+i])
 	  
-	  if(housePlayers[player].hasOwnProperty("sanma")) {
-	    const { score: subtotal } = housePlayers[player].sanma.overall
+	  if(data[player].house.three.overall.hasOwnProperty("score")) {
+	    const { score: subtotal } = data[player].house.three.overall
 
-	    housePlayers[player].sanma.overall.data.push({
+	    data[player].house.three.overall.data.push({
 	      t: date,
 	      y: subtotal + score
 	    })
-	    housePlayers[player].sanma.overall.score = subtotal + score
-	    housePlayers[player].sanma.overall.count += 1
+	    data[player].house.three.overall.score = subtotal + score
+	    data[player].house.three.overall.count += 1
 	  } else {
-	    housePlayers[player].sanma = {
-	      overall: { 
+	    data[player].house.three.overall = { 
 		data: [{
 		  t: new Date(Date.parse("01-15-2017")),
 		  y: 0
@@ -169,6 +188,37 @@ class MahjongPage extends Component {
 		score,
 		count: 1
 	      }
+	    }
+	}
+      })
+
+      table4Man.forEach(game => {
+	const date = new Date(Date.parse(game[0]))
+
+	for( let i = 0; i < 4; i += 1 ) {
+	  const player = game[1+i]
+	  const score = Number(game[5+i])
+
+	  if(data[player].house.four.overall.hasOwnProperty("score")) {
+	    const {score: subtotal } = data[player].house.four.overall
+
+	    data[player].house.four.overall.data.push({
+	      t: date,
+	      y: subtotal + score
+	    })
+	    data[player].house.four.overall.score = subtotal + score
+	    data[player].house.four.overall.count += 1
+	  } else {
+	    data[player].house.four.overall = {
+	      data : [{
+		t: new Date(Date.parse("01-15-2017")),
+		y: 0
+	      }, {
+		t: date,
+		y: score
+	      }],
+	      score,
+	      count: 1
 	    }
 	  }
 	}
@@ -184,33 +234,33 @@ class MahjongPage extends Component {
 	scales: {
 	  xAxes: [
 	    {
-            title: "time",
-            type: "time",
-            time: {
-              unit: "month",
-              unitStepSize: 1,
-              displayFormats: {
-                millisecond: 'MMM YY',
-                          second: 'MMM YY',
-                          minute: 'MMM YY',
-                          hour: 'MMM YY',
-                          day: 'MMM YY',
-                          week: 'MMM YY',
-                          month: 'MMM YY',
-                          quarter: 'MMM YY',
-                          year: 'MMM YY',
-               }
-            }
-          }
+	      title: "time",
+	      type: "time",
+	      time: {
+		unit: "month",
+		unitStepSize: 1,
+		displayFormats: {
+		  millisecond: 'MMM YY',
+		  second: 'MMM YY',
+		  minute: 'MMM YY',
+		  hour: 'MMM YY',
+		  day: 'MMM YY',
+		  week: 'MMM YY',
+		  month: 'MMM YY',
+		  quarter: 'MMM YY',
+		  year: 'MMM YY',
+		}
+	      }
+	    }
 	  ]
 	}
       }
 
-      const newData = Object.keys(housePlayers).reduce((previous, current) => {
-	previous.labels = housePlayers.James.sanma.overall.data.map(point => point.t ? point.t : 0)
+      const threeData = Object.keys(data).reduce((previous, current) => {
+	previous.labels = data.James.house.three.overall.data.map(point => point.t ? point.t : 0)
         previous.datasets.push({
           label: current,
-	  data: housePlayers[current].sanma.overall.data,
+	  data: data[current].house.three.overall.data,
 	  fill: false,
                   lineTension: 0.1,
         borderCapStyle: 'butt',
@@ -226,19 +276,42 @@ class MahjongPage extends Component {
             	return previous
       }, {datasets: [], labels: []})
 
-      console.log(newData)
+      console.log(data)
 
-      this.setState({datasets: newData, dataOptions: newOptions, loaded: "Created house datasets..."})
+      const fourData = Object.keys(data).reduce((previous, current) => {
+	if( data[current].house.hasOwnProperty("four") ) {
+	previous.labels = data.James.house.four.overall.data.map(point => point.t ? point.t : 0)
+	previous.datasets.push({
+	  label: current,
+	  data: data[current].house.four.overall.data,
+	  fill: false,
+	  lineTension: 0.1,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderWidth: 1,
+          pointRadius: 1,
+          hoverRadius: 1
 
-      console.log(housePlayers);
-      console.log(houseSeasons)
-      console.log(house3Man)
-      console.log(house4Man)
+	})
+	}
+	return previous
+      }, {datasets:[], labels: []})
+
+      console.log(threeData)
+      console.log(fourData)
+
+      this.setState({threeData, fourData, dataOptions: newOptions, status: "Created house datasets..."})
+
+      console.log(data);
+      console.log(table3Man)
+      console.log(table4Man)
       console.log(houseStartDate)
       console.log(houseEndDate)
     });
 
-    this.setState({ loaded: "Loading Tenhou data..." });
+    this.setState({ status: "Loading Tenhou data..." });
 
     const rawTenhou = await fetch(TENHOU_JSON_URL).then(response =>
       response.json()
@@ -246,16 +319,18 @@ class MahjongPage extends Component {
 
     console.log(rawTenhou);
 
-    this.setState({ loaded: "Done loading Tenhou" });
+    this.setState({ status: "Done loading Tenhou" });
   }
 
   render() {
-    if (this.state && this.state.datasets) {
+    if (this.state && this.state.threeData) {
       return (
         <Layout location={this.props.location}>
           <Helmet title={`Mahjong | ${config.siteTitle}`} />
-          <div>{this.state.loaded}</div>
-          <Line options={this.state.dataOptions} data={this.state.datasets} />
+          <div>{this.state.status}</div>
+          <div>{this.state.error? this.state.error : "No errors!"}</div>
+          <Line options={this.state.dataOptions} data={this.state.threeData} />
+          <Line options={this.state.dataOptions} data={this.state.fourData} />
         </Layout>
       );
     }
