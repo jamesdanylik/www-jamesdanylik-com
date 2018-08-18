2d32b89cfb3f4e8f7a7fb3bd4b77e722f22ae605
james <james.danylik@gmail.com>
2018-08-17 20:36:05 -0700

    Draft mahjong chart

--- src/pages/mahjong.jsx
+++ src/pages/mahjong.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
+import { Line } from "react-chartjs-2"
 
 require("es6-promise").polyfill();
 require("isomorphic-fetch");
@@ -32,18 +33,11 @@ class MahjongPage extends Component {
   async fetchSheetScores() {
     const MAHJONG_CSV_URL =
       "https://docs.google.com/spreadsheets/d/e/2PACX-1vTnhaUOyUmX4o7bQf1nUcWNr37WcQR80S7_fU4_exvwXBXU7QXTHVtwaJv5Q2qWlk6oEDH2jDDEW3Vw/pub?gid=869579873&single=true&output=csv";
-
-    // const TENHOU_JSON_URL = "https://nodocchi.moe/api/listuser.php?name=0ddba11"
-    // Had to set up a proxy to add Cross Origin Header... sillyness
-    // Neet to upgrade to HTTPS
-    // Also needs to work for STEAM IMAGES
     const TENHOU_JSON_URL =
       "https://proxy.danylik.com/nodocchi/api/listuser.php?name=0ddba11";
 
-    const rawCells = await fetch(MAHJONG_CSV_URL);
-    const rawText = await rawCells.text();
-
-    parse(rawText, (err, data) => {
+    // Process response from House Google Sheet
+    parse(await (await fetch(MAHJONG_CSV_URL)).text(), (err, data) => {
       if (err) {
         console.log("Error parsing csv");
         return;
@@ -65,7 +59,183 @@ class MahjongPage extends Component {
       //  3. calculate dan point score
       //  4. calculate R score
 
-      console.log(data);
+      const houseBalance = {}
+      const housePlayers = {}
+      const houseSeasons = []
+
+      // These will be set to the date of the first and last games
+      let houseStartDate = new Date(Date.parse("07-16-2089"))
+      let houseEndDate = new Date(Date.parse("07-16-1989"))
+
+      const house4Man = []
+      const house3Man = []
+
+      // First pass:
+      //   Collect player names + tenhou names
+      //   Split into logical tables
+      data.forEach(row => {
+	const [pHouseName, pTenhouName ] = row.slice(-2)
+	const [seasonStart, seasonEnd ] = row.slice(-4, -2)
+	const game4Man = row.slice(7, 16)
+	const game3Man = row.slice(0, 7)
+
+
+	if( pHouseName !== "" && pHouseName !== "House Player" ) {
+	  if( housePlayers.hasOwnProperty( pHouseName ) ) {
+	    if( ! housePlayers[pHouseName].hasOwnProperty("tenhou") ) {
+	      housePlayers[pHouseName].tenhou = [ pTenhouName ]
+	    } else {
+	      housePlayers[pHouseName].tenhou.push(pTenhouName)
+	    }
+	  } else {
+	    housePlayers[pHouseName] = {
+	      tenhou: [ pTenhouName ]
+	    }
+	  }
+	}
+
+	if( seasonStart !== "" && seasonStart !== "Start Date" ) {
+	  houseSeasons.push({
+	    start: new Date(Date.parse(seasonStart)),
+	    end: new Date(Date.parse(seasonEnd))
+	  })
+	}
+
+	if( game4Man[0] !== "" && game4Man[0] !== "Time" ) {
+	  const gameDate = new Date(Date.parse(game4Man[0]))
+	  if( gameDate > houseEndDate ) {
+	    houseEndDate = gameDate
+	  }
+
+	  if( gameDate < houseStartDate ) {
+	    houseStartDate = gameDate
+	  }
+
+	  game4Man.slice(1, 5).forEach(player => {
+	    if( !housePlayers.hasOwnProperty(player) ) {
+	      housePlayers[player] = {}
+	    }
+	  })
+
+	  house4Man.push(game4Man)
+	}
+
+	if ( game3Man[0] !== "" && game3Man[0] !== "Time" ) {
+	  const gameDate = game3Man[0] !== new Date(Date.parse(game3Man[0]))
+	  if( gameDate > houseEndDate ) {
+	    houseEndDate = gameDate
+	  }
+
+	  if( gameDate < houseStartDate ) {
+	    houseStartDate = gameDate
+	  }
+
+	  game3Man.slice(1, 4).forEach(player => {
+	    if( !housePlayers.hasOwnProperty(player) ) {
+	      housePlayers[player] = {}
+	    }
+	  })
+
+	  house3Man.push(game3Man)
+	}
+      })
+
+      house3Man.forEach(game => {
+	const date = new Date(Date.parse(game[0]))
+
+	for( let i = 0; i < 3; i += 1 ) {
+	  const player = game[1+i]
+	  const score = Number(game[4+i])
+	  
+	  if(housePlayers[player].hasOwnProperty("sanma")) {
+	    const { score: subtotal } = housePlayers[player].sanma.overall
+
+	    housePlayers[player].sanma.overall.data.push({
+	      t: date,
+	      y: subtotal + score
+	    })
+	    housePlayers[player].sanma.overall.score = subtotal + score
+	    housePlayers[player].sanma.overall.count += 1
+	  } else {
+	    housePlayers[player].sanma = {
+	      overall: { 
+		data: [{
+		  t: new Date(Date.parse("01-15-2017")),
+		  y: 0
+		}, {
+		  t: date,
+		  y: score
+		}],
+		score,
+		count: 1
+	      }
+	    }
+	  }
+	}
+      })
+
+      const newOptions = {
+	hover: {
+	  mode: "x",
+	},
+	tooltips: {
+	  mode: "x",
+	},
+	scales: {
+	  xAxes: [
+	    {
+            title: "time",
+            type: "time",
+            time: {
+              unit: "month",
+              unitStepSize: 1,
+              displayFormats: {
+                millisecond: 'MMM YY',
+                          second: 'MMM YY',
+                          minute: 'MMM YY',
+                          hour: 'MMM YY',
+                          day: 'MMM YY',
+                          week: 'MMM YY',
+                          month: 'MMM YY',
+                          quarter: 'MMM YY',
+                          year: 'MMM YY',
+               }
+            }
+          }
+	  ]
+	}
+      }
+
+      const newData = Object.keys(housePlayers).reduce((previous, current) => {
+	previous.labels = housePlayers.James.sanma.overall.data.map(point => point.t ? point.t : 0)
+        previous.datasets.push({
+          label: current,
+	  data: housePlayers[current].sanma.overall.data,
+	  fill: false,
+                  lineTension: 0.1,
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderWidth: 1,
+	  pointRadius: 1,
+	  hoverRadius: 1
+
+	})
+
+            	return previous
+      }, {datasets: [], labels: []})
+
+      console.log(newData)
+
+      this.setState({datasets: newData, dataOptions: newOptions, loaded: "Created house datasets..."})
+
+      console.log(housePlayers);
+      console.log(houseSeasons)
+      console.log(house3Man)
+      console.log(house4Man)
+      console.log(houseStartDate)
+      console.log(houseEndDate)
     });
 
     this.setState({ loaded: "Loading Tenhou data..." });
@@ -80,27 +250,12 @@ class MahjongPage extends Component {
   }
 
   render() {
-    if (this.state) {
+    if (this.state && this.state.datasets) {
       return (
         <Layout location={this.props.location}>
           <Helmet title={`Mahjong | ${config.siteTitle}`} />
           <div>{this.state.loaded}</div>
-          <svg
-            style={{ display: "block" }}
-            width={this.state.width}
-            height={this.state.height}
-            viewBox={`0 0 ${this.state.width} ${this.state.height}`}
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <line
-              x1="0"
-              y1={this.state.height}
-              x2={this.state.width}
-              y2="0"
-              key="line-0"
-              stroke="black"
-            />
-          </svg>
+          <Line options={this.state.dataOptions} data={this.state.datasets} />
         </Layout>
       );
     }
