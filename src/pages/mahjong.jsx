0881b2bbbd8f046f08e4efb01df0c3940157f41e
james <james.danylik@gmail.com>
2018-08-25 21:28:32 -0700

    Add working dropdowns for room & type to Mahjong

--- src/pages/mahjong.jsx
+++ src/pages/mahjong.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
 import { Line } from "react-chartjs-2";
-import Dropdown from "react-dropdown"
-
+import Dropdown from "react-dropdown";
 
 require("es6-promise").polyfill();
 require("isomorphic-fetch");
@@ -18,39 +17,10 @@ class MahjongPage extends Component {
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     console.log("constructor");
 
-    const buttons = (
-      <div>
-        <div>
-          {["house", "tenhou"].map(room => (
-            <button
-              key={room}
-              onClick={() => {
-                this.changeRoom(room);
-              }}
-            >
-              {room}
-            </button>
-          ))}
-        </div>
-        <div>
-          {["four", "three"].map(type => (
-            <button
-              key={type}
-              onClick={() => {
-                this.changeType(type);
-              }}
-            >
-              {type}
-            </button>
-          ))}
-        </div>
-      </div>
-    );
-
+ 
     this.state = {
-      activeRoom: "house",
-      types: ["three", "four"],
-      buttons
+      activeRoom: "all",
+      activeType: "all",
     };
   }
 
@@ -58,93 +28,102 @@ class MahjongPage extends Component {
     console.log("componentWillMount");
   }
 
-  changeRoom(room) {
-    console.log(`Change room: ${room}`);
-    this.setState({ activeRoom: room, test: new Date() });
-  }
-
-  changeType(type) {
-    this.setState({ activeType: type, test: new Date() });
-  }
-
-  _onSelectRoom(option) {
-    this.setState({activeRoom: option})
-  }
-
-  _onSelectType(option) {
-    this.setState({activeType: option})
-  }
-
-  _onSelectSeason(option) {
-    this.setState({activeSeason: option})
-  }
+ 
 
   getSeasons() {
-const seasons = []
-	let start = new Date(Date.parse("01/01/17"))
-	let end = new Date(Date.parse("03/01/17"))
-	const now = new Date()
+    const seasons = [];
+    let start = new Date(Date.parse("01/01/17"));
+    let end = new Date(Date.parse("03/01/17"));
+    const now = new Date();
 
-	while(end < now) {
-	  seasons.push({
-	    start,
-	    end,
-	    name: "",
-	    data: {}
-	  })
-	  start = end
-	  end = new Date(start.setMonth(start.getMonth() + 3))
-	}
-    return seasons
+    while (end < now) {
+      seasons.push({
+        start,
+        end,
+        name: "",
+        data: {}
+      });
+      start = end;
+      end = new Date(start.setMonth(start.getMonth() + 3));
+    }
+    return seasons;
   }
 
-  createGraphDataset() {
+  createGraphDataset(activeRoom, activeType) {
+    console.log(`${activeRoom} ${activeType}`)
     const graphData = {
-      labels: this.state.data.players.James.house.three.overall.data.map(p => (p.t ? p.t : 0)),
+      labels: this.state.data.players.James.house.three.overall.data.map(
+        p => (p.t ? p.t : 0)
+      ),
       datasets: []
-    }
+    };
 
-    const rooms = this.state.activeRoom !== "all" ? [this.state.activeRoom] : ["house", "tenhou"]
-    const types = this.state.activeType !== "all" ? [this.state.activeType] : ["four", "three"]
-
+    const rooms =
+      activeRoom !== "all"
+        ? [activeRoom]
+        : ["house", "tenhou"];
+    const types =
+      activeType !== "all"
+        ? [activeType]
+        : ["four", "three"];
 
     Object.keys(this.state.data.players).forEach(player => {
-      const aliasCopy = Object.keys(this.state.data.players[player])
+      const aliasCopy = Object.keys(this.state.data.players[player]);
 
-      console.log(`processing ${player}`)
-      if(this.state.activeRoom === "house") {
-	for(const a in aliasCopy) {
-	  if(aliasCopy[a] !== "house") {
-	    delete aliasCopy[a]
-	  }
-	}
-      } else if (this.state.activeRoom === "tenhou") {
-	delete aliasCopy.house
+      if (activeRoom === "house") {
+        for (const a in aliasCopy) {
+          if (aliasCopy[a] !== "house") {
+            delete aliasCopy[a];
+          }
+        }
+      } else if (activeRoom === "tenhou") {
+        delete aliasCopy.house;
       } else {
-	console.log("All")
       }
-      
+
       aliasCopy.forEach(alias => {
-	types.forEach(type => {
-	
-	if(this.state.data.players[player][alias][type]) {
-	if(this.state.data.players[player][alias][type] &&
-	   this.state.data.players[player][alias][type].overall.data) {
-	  graphData.datasets.push({
-	    label: `${player} (${alias})`,
-	    data: this.state.data.players[player][alias][type].overall.data,
-	    fill: false,
-	    lineTension: 0.1
-	  })
-	}
-	}
-	})
-      })
+        types.forEach(type => {
+          if (this.state.data.players[player][alias][type]) {
+            if (
+              this.state.data.players[player][alias][type] &&
+              this.state.data.players[player][alias][type].overall.data
+	    ) {
+	      if(activeRoom === "tenhou" && alias === "house") {
+		return
+	      }
+              graphData.datasets.push({
+                label: `${player} (${alias})`,
+                data: this.state.data.players[player][alias][type].overall.data,
+                fill: false,
+                lineTension: 0.1
+	      });
+            }
+          }
+        });
+      });
+    });
+    console.log(graphData);
 
-    })
-    console.log(graphData)
+    const dropdowns = (
+      <div>
+        <Dropdown
+          options={["house", "tenhou", "all"]}
+          value={activeRoom}
+          onChange={(option) => {
+	  this.createGraphDataset(option.value, activeType)
+	}}
+        />
+        <Dropdown
+          options={["three", "four", "all"]}
+          value={activeType}
+          onChange={(option) => {
+	  this.createGraphDataset(activeRoom, option.value)
+	}}
+        /> 
+      </div>
+    )
 
-    this.setState({graphData})
+    this.setState({ graphData, dropdowns, test: new Date(), activeRoom, activeType });
   }
 
   componentDidMount() {
@@ -152,13 +131,12 @@ const seasons = []
     window.addEventListener("resize", this.updateWindowDimensions);
     console.log("componentDidMount");
 
-
     // This is the main data object
     const data = {
-      players: {},
+      players: {}
     };
 
-    const seasons = this.getSeasons()
+    const seasons = this.getSeasons();
 
     // Helper function to return all the tenhou usernames we know about
     const getTenhouUsers = () => {
@@ -193,13 +171,13 @@ const seasons = []
     };
 
     const getGameSeason = date => {
-      for(let i = 0; i < seasons.length; i += 1) {
-	if(seasons[i].start < date && seasons[i].end > date) {
-	  return i
-	}
+      for (let i = 0; i < seasons.length; i += 1) {
+        if (seasons[i].start < date && seasons[i].end > date) {
+          return i;
+        }
       }
-      return -1
-    }
+      return -1;
+    };
 
     // Help function called on all game objects to process them
     const processGame = game => {
@@ -269,37 +247,46 @@ const seasons = []
               score,
               count: 1
             };
-	  }
+          }
 
-	  if(data.players[player][alias][word].seasons.length > 0) {
-	    const seasonId = getGameSeason(date)
+          if (data.players[player][alias][word].seasons.length > 0) {
+            const seasonId = getGameSeason(date);
 
-	    if(data.players[player][alias][word].seasons[seasonId].data.score) {
-	      const {score: subtotal } = data.players[player][alias][word].seasons[seasonId].data
+            if (
+              data.players[player][alias][word].seasons[seasonId].data.score
+            ) {
+              const { score: subtotal } = data.players[player][alias][
+                word
+              ].seasons[seasonId].data;
 
-	      data.players[player][alias][word].seasons[seasonId].data.data.push({
-		t: date,
-		y: subtotal + score
-	      })
-	      data.players[player][alias][word].seasons[seasonId].data.score = subtotal + score
-	      data.players[player][alias][word].seasons[seasonId].data.count += 1
-	    } else {
-	      data.players[player][alias][word].seasons[seasonId].data = {
-		data: [
-		  { 
+              data.players[player][alias][word].seasons[
+                seasonId
+              ].data.data.push({
+                t: date,
+                y: subtotal + score
+              });
+              data.players[player][alias][word].seasons[seasonId].data.score =
+                subtotal + score;
+              data.players[player][alias][word].seasons[
+                seasonId
+              ].data.count += 1;
+            } else {
+              data.players[player][alias][word].seasons[seasonId].data = {
+                data: [
+                  {
                     t: new Date(Date.parse("01-15-2017")),
                     y: 0
                   },
-                  { 
+                  {
                     t: date,
                     y: score
                   }
-		],
-		score,
-		count: 1
-	      }
-	    }
-	  }
+                ],
+                score,
+                count: 1
+              };
+            }
+          }
         }
       });
     };
@@ -312,13 +299,12 @@ const seasons = []
       .then(resp => resp.text())
       .then(txt => parse(txt))
       .then(csv => {
-	// STAGE 2: Do first pass on the resulting concatenated tables
+        // STAGE 2: Do first pass on the resulting concatenated tables
         this.setState({ status: "Starting csv" });
         const table = [];
 
-
-	// For each row, split into component tables
-	// Build lexicon of which players exist in which rooms
+        // For each row, split into component tables
+        // Build lexicon of which players exist in which rooms
         csv.forEach(row => {
           const [pHouseName, pTenhouName] = row.slice(-2);
           const [s1, s2] = row.slice(-4, -2);
@@ -350,7 +336,10 @@ const seasons = []
           ].forEach(({ game, num, word }) => {
             if (game[0] !== "" && game[0] !== "Time") {
               game.slice(1, 1 + num).forEach(player => {
-                const skeleton = { overall: {}, seasons: JSON.parse(JSON.stringify(seasons)) };
+                const skeleton = {
+                  overall: {},
+                  seasons: JSON.parse(JSON.stringify(seasons))
+                };
                 if (data.players[player]) {
                   if (data.players[player].house) {
                     data.players[player].house[word] = skeleton;
@@ -376,7 +365,7 @@ const seasons = []
         return getTenhouUsers();
       })
       .then(aliases =>
-	// STAGE 3: Fetch all tenhou results via proxy and wait for all to return
+        // STAGE 3: Fetch all tenhou results via proxy and wait for all to return
         Promise.all(
           aliases.map(async alias =>
             fetch(
@@ -389,7 +378,7 @@ const seasons = []
         Promise.all(responses.map(async response => response.json()))
       )
       .then(responses => {
-	// STAGE 4: First pass on tenhou data
+        // STAGE 4: First pass on tenhou data
         let collected = [];
         responses.forEach(response => {
           collected = collected.concat(response.list);
@@ -429,7 +418,7 @@ const seasons = []
         return data.table.concat(collected);
       })
       .then(table => {
-	// STAGE 5: Process games by feeding to processGame
+        // STAGE 5: Process games by feeding to processGame
         this.setState({ status: "Starting game processing..." });
         table.forEach(game => {
           processGame(game);
@@ -437,71 +426,17 @@ const seasons = []
 
         delete data.table;
 
-
-	// STAGE 6: Create graph datasets
-        data.graphs = {};
-        const rooms = ["house", "tenhou"];
-        const gameTypes = [{ word: "three", num: 3 }, { word: "four", num: 4 }];
-        rooms.forEach(room => {
-          data.graphs[room] = {};
-          gameTypes.forEach(({ word, num }) => {
-            data.graphs[room][word] = {
-              labels: data.players.James.house.three.overall.data.map(
-                point => (point.t ? point.t : 0)
-              ),
-              datasets: []
-            };
-
-            Object.keys(data.players).forEach(player => {
-              if (room === "tenhou") {
-                const aliasCopy = Object.keys(data.players[player]);
-                delete aliasCopy.house;
-
-                aliasCopy.forEach(alias => {
-                  if (
-                    data.players[player][alias][word] &&
-                    data.players[player][alias][word].overall.data &&
-                    alias !== "house"
-                  ) {
-                    data.graphs[room][word].datasets.push({
-                      label: `${player} (${alias})`,
-                      data: data.players[player][alias][word].overall.data,
-                      fill: false,
-                      lineTension: 0.1
-                    });
-                  }
-                });
-              } else if (room === "house") {
-                if (
-                  data.players[player][room][word] &&
-                  data.players[player][room][word].overall.data
-                ) {
-                  data.graphs[room][word].datasets.push({
-                    label: player,
-                    data: data.players[player][room][word].overall.data,
-                    fill: false,
-                    lineTension: 0.1
-                  });
-                }
-              }
-            });
-          });
-        });
-
-	console.log(data)
-
-	// DONE!!
         this.setState({
           status: "Done",
           data,
           activeRoom: "all",
           activeType: "all",
           test: new Date()
-	});
+        });
       })
       .then(() => {
-	this.createGraphDataset()
-      })
+        this.createGraphDataset(this.state.activeRoom, this.state.activeType);
+      });
   }
 
   componentWillUnmount() {
@@ -524,6 +459,7 @@ const seasons = []
       tooltips: {
         mode: "x"
       },
+      animation: false,
       scales: {
         xAxes: [
           {
@@ -554,9 +490,7 @@ const seasons = []
         <Line
           key={this.state.test}
           options={cOptions}
-          data={
-            this.state.graphData
-          }
+          data={this.state.graphData}
         />
       ) : (
         ""
@@ -572,15 +506,12 @@ const seasons = []
             <h1>
               {this.state.activeRoom} {this.state.activeType}
             </h1>
-            <Dropdown options={this.getSeasons().map(s => s.start)} />
-            <Dropdown options={["house", "three"]} value="house" />
-            <Dropdown options={["three", "four"]} value="three" />
+            {this.state.dropdowns}
           </div>
         ) : (
           ""
         )}
         {c1}
-        {this.state && this.state.data ? this.state.buttons : ""}
       </Layout>
     );
   }
